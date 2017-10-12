866fece71bd04fea66e5c6af6252bd97f4ada271
Iago Duarte <iagoduarte16@hotmail.com>
2017-10-12 14:40:27 -0300

    Corrigida a conexão com o servidor
    Corrigigo o problema do app parar de funcionar enquanto esperava resposta do servidor
    Adicionado um campo de visualização da resposta JSON (provisório)

--- app/src/main/java/iago/arkmeds/activity_MovieList.java
+++ app/src/main/java/iago/arkmeds/activity_MovieList.java
@@ -1,6 +1,7 @@
 package iago.arkmeds;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -14,50 +15,67 @@ import java.net.URL;
 
 public class activity_MovieList extends AppCompatActivity {
 
+
+    private TextView resultDisplay;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__movie_list);
-        Intent searchTerm = getIntent();
-        //TextView resultDisplay = (TextView)findViewById(R.id.textViewItem);
-
-        //Conexao com servidor OMDBAPI, URL define o tipo de busca, categoria para filme
-        // tipo de resposta para JSON e chave de acesso
-
-        HttpURLConnection connection = null;
-        BufferedReader reader = null;
-        try {
-            URL url = new URL("http://www.omdbapi.com/?s=" + searchTerm +
-                    "&type=movie&r=json&apikey=422461bb");
-            connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-            InputStream stream = connection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(stream));
-            StringBuffer buffer = new StringBuffer();
-            String currentLine ="";
-            while ((currentLine=reader.readLine()) != null){
-                buffer.append(currentLine);
-            }
-
-
-        } catch (MalformedURLException ex) {
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            if(connection != null) {
-                connection.disconnect();
-            }
-            try {
-                if (reader != null){
-                    reader.close();
-                }
-            } catch (IOException ex){
-                ex.printStackTrace();
-            }
-        }
-
-
+        Intent intent = getIntent();
+        String searchTerm = intent.getStringExtra(activity_search.EXTRA_MESSAGE);
+        resultDisplay = (TextView) findViewById(R.id.textViewItem);
+        new runInBackground().execute("http://www.omdbapi.com/?s=" + searchTerm +
+                "&type=movie&r=json&apikey=422461bb");
     }
 
+
+    //Conexão com servidor OMDBAPI que é feita em background
+    // A URL passada como parâmetro define o tipo de busca, categoria para filme
+    // tipo de resposta para JSON e chave de acesso
+    public class runInBackground extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(urls[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String currentLine = "";
+                while ((currentLine = reader.readLine()) != null) {
+                    buffer.append(currentLine);
+                }
+                return buffer.toString();
+
+            } catch (MalformedURLException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            resultDisplay.setText(result);
+        }
+    }
 }
